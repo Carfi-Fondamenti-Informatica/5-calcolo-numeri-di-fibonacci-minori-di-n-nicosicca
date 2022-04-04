7aad553a9d5adb6c48d96c4edd4235f731a4cdfd
nicosicca <100024661+nicosicca@users.noreply.github.com>
2022-04-04 08:35:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+   int t=1, ts=0, td=1;
+   cin>>n;
+   
+   do{
+      cout<<t;
+      
+      t=ts+td;
+      ts=td;
+      td=t;
+      
+   }while(t<n);
+   
    return 0;
 }
